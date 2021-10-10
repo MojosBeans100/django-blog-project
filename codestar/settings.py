a97d4ff9dc452cedf5d433ce3d8ef2279ac5dd5b
MojosBeans100 <lucy.colley@btinternet.com>
2021-10-10 10:12:59 +0000

    Fixed some tpyes

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = 'codestart2021.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestart2021.herokuapp.com', 'localhost']
 
 
 
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BAE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
